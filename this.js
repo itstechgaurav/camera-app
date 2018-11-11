c5facf562f0bed7cb636aa217e07e2e5769fb913
Gaurav Bhardwaj <myselfgroot@gmail.com>
2018-11-11 23:23:03 +0530

    init

--- this.js
+++ this.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")))
