b883ae4fa86bb723eb7aee3bbd75a4db377f9b3d
Gaurav Bhardwaj <myselfgroot@gmail.com>
2018-11-11 23:18:02 +0530

    init

--- this.js
+++ this.js
@@ -5,14 +5,14 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get('/', function (req, res) {
-  res.status(200);
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get('/favicon.ico', function (req, res) {
-  res.status(200);
-  res.sendFile(path.join(__dirname, "favicon.ico"));
-});
+//app.get('/', function (req, res) {
+//  res.status(200);
+//  res.sendFile(path.join(__dirname, "index.html"));
+//});
+//
+//app.get('/favicon.ico', function (req, res) {
+//  res.status(200);
+//  res.sendFile(path.join(__dirname, "favicon.ico"));
+//});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
